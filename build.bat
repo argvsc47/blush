430aa95540762493019f672698b7781780171af1
bioniclambda <80077386+bioniclambda@users.noreply.github.com>
2021-06-26 16:54:31 -0700

    Add build scripts
    the build.bat should work

---
+++ build.bat
@@ -0,0 +1,4 @@
+pip install pyinstaller
+pyinstaller --onefile main.py
+
+echo You should be able to find the blush executable in dist/
---
+++ build.sh
@@ -0,0 +1,4 @@
+sudo pip3 install pyinstaller
+sudo pyinstaller --onefile main.py
+
+echo You can find the blush executable in dist/
